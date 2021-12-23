08172edd427e1c1d04a701fe9100d44f3f264512
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2021-12-23 12:13:00 +0000

    Removed Not Needed Code

--- test.py
+++
@@ -1,5 +0,0 @@
-with open("new_file", "w") as f:  # create a new file in the current directory
-    f.write("Hello world!")       # write something
-
-with open("new_file", "r") as f:  # read the same file later on
-    print(f.read())               # print the contents to console
